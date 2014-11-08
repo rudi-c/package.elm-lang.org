68af01d1e3e9e4e1540105950f5fd8b1d73c698b
rudi-c <rudichen@gmail.com>
2014-11-07 22:07:15 -0800

    Implement simple filter search based.

--- apiserver/app/controllers/SearchEngine.scala
+++ apiserver/app/controllers/SearchEngine.scala
@@ -13,14 +13,41 @@ object SearchEngine extends Controller {
   }
 
   def listLibraries = Action {
-    Ok("Listing...")
+    val libs = Documentation.libraries.keys mkString("\n")
+    Ok(libs)
   }
 
   def getLibraryByName(name : String) = Action {
-    Ok("Retrieving...")
+    val json = Documentation.libraries.get(name) match {
+      case Some(library) => library.json.toString
+      case None => "Error no library found."
+    }
+    Ok(json)
   }
 
-  def searchLibraries(name : String) = Action {
-    Ok("Searching...")
+  def searchLibraries(query : String) = Action {
+    val results = Documentation.libraries.values
+    .map { library =>
+      val matches = library.values
+        .map { _.name }
+        .filter (isSubsequence(query))
+      (library.name, matches)
+    } .filter { case (libraryName, matches) =>
+      isSubsequence(query)(libraryName) || matches.length > 0
+    }
+
+    Ok(results mkString "\n")
+  }
+
+  def isSubsequence(query: String) (sequence: String): Boolean = {
+    def helper(query: List[Char], sequence: List[Char]): Boolean = {
+      (query, sequence) match {
+        case (Nil, _) => true
+        case (_, Nil) => false
+        case (x::xs, y::ys) if x == y => helper(xs, ys)
+        case (xs, y::ys) => helper(xs, ys)
+      }
+    }
+    helper(query.toList, sequence.toList)
   }
 }
